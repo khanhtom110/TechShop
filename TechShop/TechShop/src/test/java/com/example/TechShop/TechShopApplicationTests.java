d1122777bfcfdca455e1635455ce9df059735841
Nguyen Viet Khanh <lemontre3x1@gmail.com>
2026-05-12 21:19:21 +0700

    Initial commit - TechShop project

---
+++ TechShop/TechShop/src/test/java/com/example/TechShop/TechShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TechShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
